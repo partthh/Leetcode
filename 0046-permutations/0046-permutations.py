5b40e9111a4a9166fe44309c529a8cd237620a62
partthh <145384136+partthh@users.noreply.github.com>
2024-08-20 03:07:44 +0530

    Time: 23 ms (40.49%), Space: 11.9 MB (39.69%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,15 +1,31 @@
-class Solution:
-    def backtracking(self,res,visited,subset,nums):
-        if len(subset) == len(nums):
-            res.append(subset)
+class Solution(object):
+    def __init__(self):
+        self.result=[]
+    def solve(self,nums,current,used):
+        if(len(current)==len(nums)):
+            self.result.append(current[:])
+            return
         for i in range(len(nums)):
-            if i not in visited:
-                visited.add(i)
-                self.backtracking(res,visited,subset+[nums[i]],nums)
-                visited.remove(i)
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        visited = set()
-        res = []
-        self.backtracking(res,visited,[],nums)
-        return res
-    
+            if(nums[i] not in current):
+                current.append(nums[i])
+                used[i]=True
+                self.solve(nums,current,used)
+                used[i]=False
+                current.pop()
+                # break
+        
+        
+        # current.append(nums[index])
+        
+        # current.pop()
+        # self.solve(nums,current,index+1)
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        self.result=[]
+        used=[False]*len(nums)
+        self.solve(nums,[],used)
+        return self.result
+        
