4a3dc63f340a163d540da905c460c12d8340606b
partthh <145384136+partthh@users.noreply.github.com>
2024-08-23 20:36:30 +0530

    Time: 17 ms (38.65%), Space: 11.6 MB (67.15%) - LeetHub

---
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def minBitFlips(self, start, goal):
+        """
+        :type start: int
+        :type goal: int
+        :rtype: int
+        """
+        x1=bin(start^goal)
+        return x1.count("1")
