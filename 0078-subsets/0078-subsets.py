9066a32fd8c7b86e9190834401015c5d42c25f1d
partthh <145384136+partthh@users.noreply.github.com>
2024-09-02 03:07:21 +0530

    Time: 33 ms (80.87%), Space: 16.7 MB (26.05%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,24 +1,11 @@
-class Solution(object):
-    def __init__(self):
-        self.result = []
-    
-    def solve(self, nums, index, curr):
-        if index == len(nums):
-            self.result.append(curr[:]) 
-            return
-        
-        curr.append(nums[index])
-        self.solve(nums, index + 1, curr)
-        
-        curr.pop()
-        self.solve(nums, index + 1, curr)
-
-    def subsets(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        self.result = []
-        self.solve(nums, 0, [])
-        return self.result
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        for i in range(1<<len(nums)):
+            sublist = []
+            for j in range(len(nums)):
+                if (i&(1<<j)):
+                    sublist.append(nums[j])
+            ans.append(sublist.copy())
+        return ans
         
