4a53e981d88345b7b3eb5e8864674b3198be03db
partthh <145384136+partthh@users.noreply.github.com>
2024-09-15 03:13:38 +0530

    Time: 140 ms (9.92%), Space: 14.9 MB (89.65%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -7,11 +7,12 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        l1=[]
+        # l1=[]
         x1=Counter(nums)
-        for i,f in x1.most_common(k):
-            l1.append(i)
-        return l1
+        return heapq.nlargest(k,x1.keys(),key=x1.get)
+        # for i,f in x1.most_common(k):
+        #     l1.append(i)
+        # return l1
 
         
         
