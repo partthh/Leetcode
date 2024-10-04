883bd0d22f50fd0af921f1e11af1774822150db0
partthh <145384136+partthh@users.noreply.github.com>
2024-10-05 02:23:51 +0530

    Time: 43 ms (7.66%), Space: 19.4 MB (64.38%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -1,22 +1,33 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        res = []
+class Solution(object):
+    def __init__(self):
+        self.ans=[]
+    def helper(self,root):
+        if not root:
+            return None
         
-        def bst(node):
-            if (not node or (len(res) >= k)):
-                return
-            
-            bst(node.left)
-            res.append(node.val)
-            bst(node.right)
-            
-        bst(root)
-        
-        return res[k - 1]
-        
+        self.helper(root.left)
+        self.ans.append(root.val)
+        self.helper(root.right)
+            # elif( root.right):
+            #     root=self.kthSmallest(root.right,k)
+        return self.ans
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        # ans=set()
+        if not root:
+            return None
+        arr1=[]
+        arr1=self.helper(root)
+        arr1.sort()
+        x=arr1[k-1]
+        return x
