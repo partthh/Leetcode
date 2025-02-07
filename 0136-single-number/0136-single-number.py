6b54dc8e2a5e773b6583696e1ef78a1d6d68a8e7
partthh <145384136+partthh@users.noreply.github.com>
2025-02-07 23:20:34 +0530

    Time: 15 ms (29.91%), Space: 15.3 MB (6.01%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -4,8 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        ans=0
-        for i in nums:
-            ans^=i
-        return ans
-        
+        n=len(nums)
+        hashset={}
+        for i in range(n):
+            if(nums[i] in hashset):
+                hashset[nums[i]]+=1
+            else:
+                hashset[nums[i]]=1
+        for key,value in hashset.items():
+            if(value==1):
+                return key       
