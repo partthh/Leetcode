85b33813c71e2d32d9c2485f91d2b8895363b51b
partthh <145384136+partthh@users.noreply.github.com>
2025-02-08 17:53:44 +0530

    Time: 0 ms (100%), Space: 13 MB (73.45%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,18 +5,8 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        
-        
-        for i in range(len(nums)):
-            l1=[]
-            sum1=0
-            sum1+=nums[i]
-            l1.append(i)
-            for j in range(i+1,len(nums)):
-                sum1+=nums[j]
-
-                if sum1==target:
-                    l1.append(j)
-                    return l1
-                else:
-                    sum1-=nums[j]
+        dict1={}
+        for i ,num in enumerate(nums):
+            if target-num in dict1:
+                return [i,dict1[target-num]]
+            dict1[num]=i
