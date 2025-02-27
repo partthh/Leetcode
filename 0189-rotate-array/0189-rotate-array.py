f229b37e40ca3072f9d8ff67165905c04c1b250e
partthh <145384136+partthh@users.noreply.github.com>
2025-02-27 12:41:40 +0530

    Time: 0 ms (100%), Space: 22.8 MB (37.11%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -5,17 +5,7 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-
-        n=len(nums)
-        k=(n-k)%n
-        arr=[]
-        pos=0
-        for i in range(k):
-            arr.append(nums[i])
-        for i in range(k,n):
-            nums[pos]=nums[i]
-            pos+=1
-        pos=0
-        for i in range(n-k,n):
-            nums[i]=arr[pos]
-            pos+=1
+        k=k%len(nums)
+        nums.reverse()
+        nums[:k]=reversed(nums[:k])
+        nums[k:]=reversed(nums[k:])
