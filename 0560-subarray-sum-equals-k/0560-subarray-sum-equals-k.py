20a45cd8f5b8a513274fbfbb580dd1f085bae683
partthh <145384136+partthh@users.noreply.github.com>
2025-02-28 16:16:20 +0530

    Time: 35 ms (67.5%), Space: 15.3 MB (29.57%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,25 +1,20 @@
 class Solution(object):
-    def subarraySum(self, arr, k):
+    def subarraySum(self, nums, k):
         """
         :type nums: List[int]
         :type k: int
         :rtype: int
         """
-        hash1={}
-        sum1=0
+        hashset={0:1}
         count=0
-        for i in range(len(arr)):
-            sum1+=arr[i]
-            if sum1==k:
-                count+=1
-            if sum1-k in hash1:
-                # maxi=max(maxi,length)
-                count+=hash1[sum1-k]
-
-            if sum1 not in hash1:
-                hash1[sum1]=1
-            # if hash1[sum1] not in 
+        sum1=0
+        for i in range(len(nums)):
+            sum1+=nums[i]
+            if sum1-k in hashset:
+                count+=hashset[sum1-k]
+            if sum1 in hashset:
+                hashset[sum1]+=1
             else:
-                hash1[sum1]=hash1[sum1]+1
+                hashset[sum1]=1
         return count
         
