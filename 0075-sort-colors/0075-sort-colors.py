dea34099a8720110d73a663e4b2b9b89d3d8105e
partthh <145384136+partthh@users.noreply.github.com>
2025-03-01 13:28:48 +0530

    Time: 0 ms (100%), Space: 12.5 MB (11.14%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,10 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
+        low=0
+        mid=0
+        high=len(nums)-1
+        while mid<=high:
+            if(nums[mid]==0):
+                nums[low],nums[mid]=nums[mid],nums[low]
+                mid+=1
+                low+=1
+            elif(nums[mid]==1):
+                mid+=1
+            else:
+                nums[mid],nums[high]=nums[high],nums[mid]
+                high-=1
 
-        for i in range(len(nums)-1):
-            for j in range(i+1,len(nums)):
-                if nums[i]>nums[j]:
-                    nums[i],nums[j]=nums[j],nums[i]
-        return nums
-        
+                
