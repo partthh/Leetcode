b5175ff2c2f20605a16e8ae06a02ad38f4980fe0
partthh <145384136+partthh@users.noreply.github.com>
2025-03-12 23:37:21 +0530

    Time: 2 ms (84.47%), Space: 17.9 MB (21.26%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,24 +1,17 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        result = 0
-        dictu = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        n = len(s)
-        a = 0
-        b = a + 1
-        while b <= n - 1:
-            if s[a] == 'X' and (s[b] == 'L' or s[b] == 'C'):
-                result += dictu[s[b]] - dictu[s[a]]
-                a += 2
-            elif s[a] == 'C' and (s[b] == 'D' or s[b] == 'M'):
-                result += dictu[s[b]] - dictu[s[a]]
-                a += 2
-            elif s[a] == 'I' and (s[b] == 'V' or s[b] == 'X'):
-                result += dictu[s[b]] - dictu[s[a]]
-                a += 2
+        add1=0
+        set1={"I":1,'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000}
+        for i,j in zip(s,s[1:]):
+            if set1[i]<set1[j]:
+                add1-=set1[i]
+            # elif(set1[i]>set1[j]):
+                # pass
             else:
-                result += dictu[s[a]]
-                a += 1
-            b = a + 1
-        if a == n - 1:
-            result += dictu[s[n-1]]
-        return result
+                add1+=set1[i]
+        return add1+set1[s[-1]]
