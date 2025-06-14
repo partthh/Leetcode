656934eaae5723001d74f4df210720d362508f22
partthh <145384136+partthh@users.noreply.github.com>
2025-06-15 03:54:50 +0530

    Time: 7 ms (33.71%), Space: 19.9 MB (14.41%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,16 +1,12 @@
-class Solution(object):
-    def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        set1={}
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        dict1={}
         for i in nums:
-            
-            if i in set1:
-                set1[i]+=1
-            else:
-                set1[i]=1
-        for key,value in set1.items():
+            # if i in dict1:
+            dict1[i]=dict1.get(i,0)+1
+            # else:
+            #     dict1[i]=1
+        print (dict1)
+        for i,value in dict1.items():
             if value==1:
-                return key
+                return i
