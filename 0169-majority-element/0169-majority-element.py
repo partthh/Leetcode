cc957f89acd16b62ca85c56f1885301c8423756d
partthh <145384136+partthh@users.noreply.github.com>
2025-06-19 03:11:34 +0530

    Time: 27 ms (6.67%), Space: 13.6 MB (64.54%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,21 +1,11 @@
+from collections import Counter
 class Solution(object):
     def majorityElement(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        set1={}
-        a1=[]
-        for i in nums:
-            if i in set1:
-                set1[i]+=1
-            else:
-                set1[i]=1
-        for i,values in set1.items():
-            con=len(nums)//2
-            if(values>con):
+        dict1=Counter(nums)
+        for i,j in dict1.items():
+            if 2*j>len(nums):
                 return i
-                # a1.append(i)
-            
-        # return a1
-        
