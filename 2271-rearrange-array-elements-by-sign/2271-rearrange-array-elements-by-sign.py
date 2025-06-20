43d61878ddc604c3a78dbb7454f11fda3613f365
partthh <145384136+partthh@users.noreply.github.com>
2025-06-21 04:32:34 +0530

    Time: 44 ms (95%), Space: 42.5 MB (82.74%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
@@ -4,20 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        l1=[]
-        l2=[]
-        for i in range(len(nums)):
-            if nums[i]>0:
-                l1.append(nums[i])
+        pos=0
+        neg=1
+        arr1=[0]*len(nums)
+        for i in nums:
+            if i>0:
+                arr1[pos]=i
+                pos+=2
             else:
-                l2.append(nums[i])
-        pos=1
-        pos1=0
-        while len(l1)!=len(nums):
-            
-            l1.insert(pos,l2[pos1])
-            pos+=2
-            pos1+=1
-        return l1
-        # for i in range(1,len(l2)):
-            # l1.insert(i)
+                arr1[neg]=i
+                neg+=2
+        return arr1
