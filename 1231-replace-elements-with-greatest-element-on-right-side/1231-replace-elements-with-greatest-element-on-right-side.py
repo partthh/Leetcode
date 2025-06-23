74c949c3448c3b60a14faa35810b9134195417c1
partthh <145384136+partthh@users.noreply.github.com>
2025-06-24 04:25:21 +0530

    Time: 11 ms (99.3%), Space: 14 MB (55.11%) - LeetHub

--- 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.py
+++ 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.py
@@ -4,15 +4,18 @@ class Solution(object):
         :type arr: List[int]
         :rtype: List[int]
         """
-        arr1=[-1]*len(arr)
+        # arr1=[-1]*len(arr)
+        # curr=-1
         maxi=arr[-1]
         for i in range(len(arr)-2,-1,-1):
-            
-            arr1[i]=maxi
-            if arr[i]>maxi:
-                maxi=arr[i]
+            curr=arr[i]
+            arr[i]=maxi
+            if curr>maxi:
+                maxi=curr
+        arr[-1]=-1
+        return arr
 
-        return arr1
+        # return arr1
 
 
 
