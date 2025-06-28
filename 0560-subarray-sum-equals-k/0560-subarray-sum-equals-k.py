48c41804efac7afcc4241c4d4a710b293b5b02d8
partthh <145384136+partthh@users.noreply.github.com>
2025-06-29 01:42:29 +0530

    Time: 31 ms (83.37%), Space: 15.3 MB (24.61%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -5,16 +5,15 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        hashset={0:1}
         count=0
+        hash1={0:1}
         sum1=0
         for i in range(len(nums)):
             sum1+=nums[i]
-            if sum1-k in hashset:
-                count+=hashset[sum1-k]
-            if sum1 in hashset:
-                hashset[sum1]+=1
+            if sum1-k in hash1:
+                count+=hash1[sum1-k]
+            if sum1 in hash1:
+                hash1[sum1]+=1
             else:
-                hashset[sum1]=1
+                hash1[sum1]=1
         return count
-        
