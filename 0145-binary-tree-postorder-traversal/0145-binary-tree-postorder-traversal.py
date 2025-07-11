1c312c8290f03b498021aa20be0233f5607ed75d
partthh <145384136+partthh@users.noreply.github.com>
2025-07-12 03:37:06 +0530

    Time: 0 ms (100%), Space: 12.5 MB (16.54%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
@@ -5,37 +5,17 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
+    def __init__(self):
+        self.arr=[]
     def postorderTraversal(self, root):
         """
-        :type root: TreeNode
+        :type root: Optional[TreeNode]
         :rtype: List[int]
         """
-        stack1=[]
-        stack2=[]
-        postorder=[]
-
-        stack1.append(root)
-        if root is None:
-            return postorder
-        while stack1:
-            x1=stack1.pop()
-            stack2.append(x1.val)
-            if x1.left:
-                stack1.append(x1.left)
-
-            if x1.right:
-                stack1.append(x1.right)
+        if root:
+            self.postorderTraversal(root.left)
             
-        while stack2:
-            postorder.append(stack2.pop())
-        return postorder
-
-
-
-
-
-
-
-
-
+            self.postorderTraversal(root.right)
+            self.arr.append(root.val)
+        return self.arr
         
