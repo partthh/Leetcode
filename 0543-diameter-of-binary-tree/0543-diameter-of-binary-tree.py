26103a24dc5f69663f59341737f88115d79136cf
partthh <145384136+partthh@users.noreply.github.com>
2025-07-16 04:36:04 +0530

    Time: 10 ms (78.01%), Space: 24.5 MB (89.81%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -5,22 +5,30 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
+    # def __init__(self):
+        # self.maxi=0
     def diameterOfBinaryTree(self, root):
         """
-        :type root: TreeNode
+        :type root: Optional[TreeNode]
         :rtype: int
         """
-        diameter=[0]
-        self.height(root,diameter)
-        return diameter[0]
-    def height(self,node,diameter):
-        if not node:
-            return 0
-        
-        lh=self.height(node.left,diameter)
-        rh=self.height(node.right,diameter)
-        diameter[0]=max(diameter[0],lh+rh)
-        return 1+max(lh,rh)
-        # return 0
+        # if not root:
+            # return 0
+        self.maxi=0
+        self.fun1(root)
+        return self.maxi
 
+        # l1=self.diameterOfBinaryTree(root.left)
+        # r1=self.diameterOfBinaryTree(root.right)
+
+        # self.maxi=max(self.maxi,l1+r1)
+
+        # return 1+max(l1,r1)
         
+    def fun1(self,root):
+        if not root:
+            return 0
+        l1=self.fun1(root.left)
+        r1=self.fun1(root.right)
+        self.maxi=max(self.maxi,l1+r1)
+        return max(l1,r1)+1
