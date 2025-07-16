dd392ecb15fe73ff56f66c7417ea25b2c158881e
partthh <145384136+partthh@users.noreply.github.com>
2025-07-17 03:08:48 +0530

    Time: 0 ms (100%), Space: 12.5 MB (53.18%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -4,33 +4,30 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
 class Solution(object):
     def isSameTree(self, p, q):
         """
-        :type p: TreeNode
-        :type q: TreeNode
+        :type p: Optional[TreeNode]
+        :type q: Optional[TreeNode]
         :rtype: bool
         """
-        if p is None and q is None:
-            return True
-        if p is None or q is None:
-            return False
-        # l1=[]
-        # l2=[]
-        # if p:
-        #     l2.append(p.val)
-        #     self.isSameTree(p.left)
-        #     self.isSameTree(p.right)
-        
-        # if q:
-        #     l2.append(q.val)
-        #     self.isSameTree(q.left)
-        #     self.isSameTree(q.right)
-        
-        return (p.val==q.val and
-                self.isSameTree(p.left,q.left) and
-                self.isSameTree(p.right,q.right))
+        # if not p and not q:
+        #     return True
+        # if not p or not q:
+        #     return False
+        # if p.val!=q.val:
+        #     return False
+        self.p=[]
+        self.q=[]
+        self.fun1(p,self.p)
+        self.fun1(q,self.q)
+        return self.p==self.q
+    def fun1(self,root,arr):
+        if not root:
+            arr.append(None)
+            return 
+        arr.append(root.val)
+        self.fun1(root.left,arr)
+        self.fun1(root.right,arr)
 
-        
         
