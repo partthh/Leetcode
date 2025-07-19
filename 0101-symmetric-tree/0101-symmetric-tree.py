977ee340fb71cd14cd7f5995036e5c9a7c3e449d
partthh <145384136+partthh@users.noreply.github.com>
2025-07-20 05:04:09 +0530

    Time: 6 ms (3.15%), Space: 12.6 MB (57.18%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -7,21 +7,46 @@
 class Solution(object):
     def isSymmetric(self, root):
         """
-        :type root: TreeNode
+        :type root: Optional[TreeNode]
         :rtype: bool
         """
         if not root:
-            return True
-        return self.isTreeSymmetric(root.left,root.right)
-    def isTreeSymmetric(self,leftroot,rightroot):
-        # leftroot=root.left
-        # rightroot=root.right
-        if not leftroot and not rightroot:
-            return True
-        if ((not leftroot or not rightroot)):
             return False
-        if leftroot.val!=rightroot.val:
+        if not root.left and not root.right:
+            return True
+        if not root.left or not root.right:
             return False
+        if root.left.val!=root.right.val:
+            return False
+        x1=self.fun1(root.left) 
+        x2=self.fun1(root.right)
+        for i in range(len(x2)):
+            x2[i].reverse()
+        if x1!=x2:
+            return False
+        return True
+    
+    def fun1(self,root):
+        if not root:
+            return []
+        queue=deque([root])
+        res=[]
+        while queue:
+            l1=len(queue)
+            temp=[]
+            for i in range(l1):
+                n1=queue.popleft()
+                if n1:
 
-        return self.isTreeSymmetric(leftroot.left,rightroot.right) and self.isTreeSymmetric(leftroot.right,rightroot.left)
-        
+                    temp.append(n1.val)
+                    queue.append(n1.left)
+                    queue.append(n1.right)
+                else:
+                    temp.append(None)
+
+                # if n1.left:
+                #     queue.append(root.left)
+                # if n1.right:
+                #     queue.append(root.left)
+            res.append(temp)
+        return res
