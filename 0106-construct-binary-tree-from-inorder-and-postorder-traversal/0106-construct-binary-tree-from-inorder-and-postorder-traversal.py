6a8587b3acedc9aaec3a327c4ebe4dfdf5b9c6ed
partthh <145384136+partthh@users.noreply.github.com>
2025-08-02 04:54:50 +0530

    Time: 7 ms (79.98%), Space: 16.5 MB (98.23%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.py
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -5,18 +5,23 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
+    def fun1(self,postorder,inorder,start,end,dict1):
+        if start>end:
+            return None
+        root=TreeNode(postorder[self.ind])
+        # TreeNode.val=preorder[ind]
+        i=dict1[root.val]
+        self.ind-=1
+        root.right=self.fun1(postorder,inorder,i+1,end,dict1)
+        
+        root.left=self.fun1(postorder,inorder,start,i-1,dict1)
+        return root
     def buildTree(self, inorder, postorder):
         """
         :type inorder: List[int]
         :type postorder: List[int]
-        :rtype: TreeNode
+        :rtype: Optional[TreeNode]
         """
-        if inorder:
-            indexx=inorder.index(postorder.pop())
-            root=TreeNode(inorder[indexx])
-            root.right=self.buildTree(inorder[indexx+1:],postorder)
-            root.left=self.buildTree(inorder[:indexx],postorder)
-
-            return root
-            
-        
+        dict1={x:i for i,x in enumerate(inorder)}
+        self.ind=len(postorder)-1
+        return self.fun1(postorder,inorder,0,len(postorder)-1,dict1)
