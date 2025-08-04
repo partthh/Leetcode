ca1a9f816e8d2b29ce23d2badc6d1598675315be
partthh <145384136+partthh@users.noreply.github.com>
2025-08-05 04:28:56 +0530

    Time: 0 ms (100%), Space: 12.9 MB (39.78%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
@@ -5,48 +5,21 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
-    def fun1(self,root):
-
-        if not root:
-            return []
-        curr=root
-        res=[]
-        while curr:
-            if not curr.left:
-                res.append(curr)
-                curr=curr.right
-            else:
-                next1=curr.left
-                while next1.right:
-                    next1=next1.right
-                res.append(curr)
-                next1.right=curr.right
-                curr=curr.left
-        return res
-    def fun2(self,res):
-        if not res:
-            return None
-        root=TreeNode(res[0])
-        current=root
-        for i in res[1:]:
-            current.right=TreeNode(i)
-            current=current.right
-        return root
     def flatten(self, root):
         """
         :type root: Optional[TreeNode]
         :rtype: None Do not return anything, modify root in-place instead.
-        """
-        arr=self.fun1(root)
-        if not arr:
-            return 
-        for i in range(len(arr)-1):
-            arr[i].left=None
-            arr[i].right=arr[i+1]
-        # if arr:
-        arr[-1].left=None
-        arr[-1].right=None
-        
+        """    
+        curr=root
+        while curr:
+            if curr.left:
+                next1=curr.left
+                while next1.right:
+                    next1=next1.right
+                next1.right=curr.right
+                curr.right=curr.left
+                curr.left=None
+            curr=curr.right
 
 
                 
